01d72e96f7bcc3aa56be5554a0abe570b70cc8c2
Pro1000 <jhsibeizai@gmail.com>
2019-05-26 01:05:54 +0800

    V1.1
    Changlog 1.1:
    1. Fixed some minor bugs
    2. Added elevated admin permissions

--- Installer.py
+++ Installer.py
@@ -1,7 +1,10 @@
 import os
-import subprocess
-import time
+from subprocess import Popen,call
+from time import sleep
 import tkinter
+import ctypes 
+from sys import executable
+
 
 #Creating the GUI
 class Window(tkinter.Frame):
@@ -35,7 +38,6 @@ class Window(tkinter.Frame):
         self.all_btn.grid(row=2,column = 1,columnspan=2)
         self.maintext.grid(row=1,column = 1,columnspan = 2)
         
-        
 
         #Creating the buttons for the files within the Softwares file
         self.btns = []
@@ -43,7 +45,8 @@ class Window(tkinter.Frame):
         path = os.getcwd()+'\\' + "Softwares" + '\\'
         if len(files) > 0:
             for i in range(len(files)):
-                self.btns.append(tkinter.Button(self.master,text = files[i] + ' only', command = lambda: open_file(path + files[i])))
+                temp = path + files[i]
+                self.btns.append(tkinter.Button(self.master,text = files[i] ,command = lambda: open_file(temp)))
                 self.btns[i].grid(row=3+i,column = 1,columnspan=2)
 
 
@@ -52,20 +55,18 @@ class Window(tkinter.Frame):
         files2 = get_exe("Common",False)
         names_tvar_t = tkinter.StringVar(self.master)
         names_tvar="List of items in common folder\n"
+        
         for i in range(len(files2)):
             names_tvar += files2[i]+'\n'
+            
         if len(files2) > 0:
             names_tvar_t.set(names_tvar)
         else:
             names_tvar_t.set("There is nothing in the common folder")
+
+            
         self.c_lbl = tkinter.Label(self.master, height = len(files2)+2, width = 30, textvariable = names_tvar_t)
         self.c_lbl.grid(row=2,column = 8, rowspan = len(files2)+1)
-        
-
-
-        
-
-        
         
 
 def install_all_c():
@@ -99,29 +100,44 @@ def open_file(file_path):
         returns True once the file is stopped
     '''
     if file_path[-4:] == '.exe':
-        proc = subprocess.Popen(file_path)
+        proc = Popen(file_path)
     elif file_path[-4:] == '.lnk':
-        proc = subprocess.Popen(file_path,shell=True)
+        proc = Popen(file_path,shell=True)
     while proc.poll() is None:
-        time.sleep(2)
+        sleep(2)
     return True
 
 
+
+
 def main():
-    #Checking if the relavant folders exits:
-    path = os.getcwd()
-    if not os.path.isdir(path + "\\Softwares\\"):
-        os.mkdir(path + "\\Softwares\\")
-    if not os.path.isdir(path + "\\Common\\"):
-        os.mkdir(path + "\\Common\\")
+    #Function to check if there is admin permission
+    def is_admin():
+        try:
+            return ctypes.windll.shell32.IsUserAnAdmin()
+        except:
+            return False
 
+    if is_admin():
+
+        #Checking if the relavant folders exits:
+        path = os.getcwd()
+        if not os.path.isdir(path + "\\Softwares\\"):
+            os.mkdir(path + "\\Softwares\\")
+        if not os.path.isdir(path + "\\Common\\"):
+            os.mkdir(path + "\\Common\\")
+
+        
+        #Loading the tkinter window
+        root = tkinter.Tk()
+        root.resizable(True, True) 
+        app = Window(root)
+        root.mainloop()
+        
+    else:
+        # Re-run the program with admin rights
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", executable, __file__, None, 1)
     
-    #Loading the tkinter window
-    root = tkinter.Tk()
-    root.resizable(True, True) 
-    app = Window(root)
-    root.mainloop()
-
 
 
 if __name__ == '__main__':
